fcd9c0fb830a1aaf35a99ac203932f7b66621b31
YasarDogann <yasardgn99@gmail.com>
2025-05-28 20:21:47 +0300

    Ürünlerin listesi ve pagination eklendi

--- Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net9.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/obj/Debug/net9.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c8727751deadf9522dc88808a404cdb8d7672e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
